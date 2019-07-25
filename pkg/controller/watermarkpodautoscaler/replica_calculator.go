4c4e22df960c7fb847ea03003ab2bc09fcf98476
charlyF <charly@datadoghq.com>
2019-07-25 18:19:26 -0400

    fixing metrics and naming

--- pkg/controller/watermarkpodautoscaler/replica_calculator.go
+++ pkg/controller/watermarkpodautoscaler/replica_calculator.go
@@ -46,44 +46,45 @@ func (c *ReplicaCalculator) GetExternalMetricReplicas(currentReplicas int32, low
 		value.Delete(prometheus.Labels{"wpa_name": wpa.Name, "metric_name": metricName})
 		return 0, 0, time.Time{}, fmt.Errorf("unable to get external metric %s/%s/%+v: %s", wpa.Namespace, metricName, selector, err)
 	}
-	log.Info(fmt.Sprintf("Metrics is %v", metrics))
+	log.Info(fmt.Sprintf("Metrics from the External Metrics Provider: %v", metrics))
 	averaged := 1.0
 
 	if wpa.Spec.Algorithm == "average" {
 		averaged = float64(currentReplicas)
 	}
-	log.Info(fmt.Sprintf("Algorithm is %v", wpa.Spec.Algorithm))
+	log.Info(fmt.Sprintf("Algorithm is %s", wpa.Spec.Algorithm))
 
 	var sum int64
 	for _, val := range metrics {
 		sum = sum + val
 	}
 	adjustedUsage := float64(sum) / averaged
+	milliAdjustedUsage :=  adjustedUsage / 1000
 	utilization = int64(adjustedUsage)
 
-	log.Info(fmt.Sprintf("About to compare utilization %v vs LWM %v and HWM %v", utilization, lowMark, highMark))
+	log.Info(fmt.Sprintf("About to compare utilization %f vs LWM %d and HWM %d", adjustedUsage, lowMark, highMark))
 
 	adjustedHM := float64(highMark) + wpa.Spec.Tolerance*float64(highMark)
 	adjustedLM := float64(lowMark) - wpa.Spec.Tolerance*float64(lowMark)
 
  	// We do not use the abs as we want to know if we are higher than the high mark or lower than the low mark
 	if adjustedUsage > adjustedHM {
-		replicaCount = int32(math.Ceil(adjustedUsage / (float64(highMark))))
-		log.Info(fmt.Sprintf("Value is above highMark. Usage: %d", adjustedUsage))
+		replicaCount = int32(math.Ceil(float64(currentReplicas) * adjustedUsage / (float64(highMark))))
+		log.Info(fmt.Sprintf("Value is above highMark. Usage: %f. ReplicaCount %d", milliAdjustedUsage, replicaCount))
 
 	} else if adjustedUsage < adjustedLM  {
-		replicaCount = int32(math.Ceil(adjustedUsage / (float64(lowMark))))
-		log.Info(fmt.Sprintf("Value is below lowMark. Usage: %d", adjustedUsage))
+		replicaCount = int32(math.Ceil(float64(currentReplicas) * adjustedUsage / (float64(lowMark))))
+		log.Info(fmt.Sprintf("Value is below lowMark. Usage: %f ReplicaCount %d", milliAdjustedUsage, replicaCount))
 
 	} else {
 		restrictedScaling.With(prometheus.Labels{"wpa_name": wpa.Name, "metric_name": metricName}).Set(1)
-		value.With(prometheus.Labels{"wpa_name": wpa.Name, "metric_name": metricName}).Set(float64(adjustedUsage))
-		log.Info(fmt.Sprintf("Withing bounds of the watermarks. Value: %v is [%v; %v]", adjustedUsage, lowMark, highMark))
+		value.With(prometheus.Labels{"wpa_name": wpa.Name, "metric_name": metricName}).Set(float64(milliAdjustedUsage))
+		log.Info(fmt.Sprintf("Withing bounds of the watermarks. Value: %f is [%d; %d]", milliAdjustedUsage , lowMark, highMark))
 		return currentReplicas, utilization, timestamp, nil
 	}
 
 	restrictedScaling.With(prometheus.Labels{"wpa_name": wpa.Name, "metric_name": metricName}).Set(0)
-	value.With(prometheus.Labels{"wpa_name": wpa.Name, "metric_name": metricName}).Set(float64(adjustedUsage))
+	value.With(prometheus.Labels{"wpa_name": wpa.Name, "metric_name": metricName}).Set(float64(milliAdjustedUsage))
 
 	return replicaCount, utilization, timestamp, nil
 }
