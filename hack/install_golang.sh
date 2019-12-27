dbae622bfa1077f6f8d6d2ef8fab121faaac29b6
cedric lamoriniere <cedric.lamoriniere@datadoghq.com>
2019-12-27 17:27:29 +0100

    update make file
    Signed-off-by: cedric lamoriniere <cedric.lamoriniere@datadoghq.com>

--- hack/install_golang.sh
+++ hack/install_golang.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-GOVERSION=1.12.9
+GOVERSION=1.13.5
 
 mkdir -p /usr/local
 curl -Lo go$GOVERSION.linux-amd64.tar.gz https://dl.google.com/go/go$GOVERSION.linux-amd64.tar.gz && tar -C /usr/local -xzf go$GOVERSION.linux-amd64.tar.gz
