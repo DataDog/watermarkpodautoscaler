a833bf7f62b7d55ef4b0c71b9f76abdc2b9d9ef9
Corentin Chary <corentin.chary@datadoghq.com>
2024-10-16 16:13:56 +0200

    Move the mock to a test file

---
+++ controllers/datadoghq/recommender_test.go
@@ -0,0 +1,21 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package datadoghq
+
+import "time"
+
+func NewMockRecommenderClient() *RecommenderClientMock {
+	return &RecommenderClientMock{
+		ReplicaRecommendationResponse{2, 1, 3, time.Now(), "because"},
+		nil,
+	}
+}
+
+func (m *RecommenderClientMock) GetReplicaRecommendation(request *ReplicaRecommendationRequest) (ReplicaRecommendationResponse, error) {
+	return m.ReturnedResponse, m.Error
+}
+
+var _ RecommenderClient = &RecommenderClientMock{}
