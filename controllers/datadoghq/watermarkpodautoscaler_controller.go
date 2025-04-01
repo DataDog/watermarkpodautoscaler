f8c3b05b757dd18609ae7befb003c66839bfc0cc
Corentin Chary <corentin.chary@gmail.com>
2025-04-01 14:22:26 +0200

    Handle metric computations errors and don't reset conditions (#286)
    * Handle metric computations errors and don't reset conditions
    
    * Moving setTag after error handling

--- controllers/datadoghq/watermarkpodautoscaler_controller.go
+++ controllers/datadoghq/watermarkpodautoscaler_controller.go
@@ -391,13 +391,6 @@ func (r *WatermarkPodAutoscalerReconciler) reconcileWPA(ctx context.Context, log
 
 	proposedReplicas, metricName, metricStatuses, metricTimestamp, readyReplicas, stableRegime, err := r.computeReplicas(ctx, logger, wpa, currentScale)
 
-	span.SetTag("proposed_replicas", proposedReplicas)
-	span.SetTag("metric_name", metricName)
-	span.SetTag("metric_timestamp", metricTimestamp)
-	span.SetTag("ready_replicas", readyReplicas)
-	span.SetTag("stable_regime", stableRegime)
-	span.SetTag("metric_statuses", metricStatuses)
-
 	if err != nil {
 		r.setCurrentReplicasInStatus(wpa, currentReplicas)
 		if err2 := r.updateStatusIfNeeded(ctx, wpaStatusOriginal, wpa); err2 != nil {
@@ -410,6 +403,14 @@ func (r *WatermarkPodAutoscalerReconciler) reconcileWPA(ctx context.Context, log
 		logger.Info("Failed to compute desired number of replicas based on listed metrics.", "reference", reference, "error", err)
 		return nil
 	}
+
+	span.SetTag("proposed_replicas", proposedReplicas)
+	span.SetTag("metric_name", metricName)
+	span.SetTag("metric_timestamp", metricTimestamp)
+	span.SetTag("ready_replicas", readyReplicas)
+	span.SetTag("stable_regime", stableRegime)
+	span.SetTag("metric_statuses", metricStatuses)
+
 	logger.Info("Proposing replicas", "proposedReplicas", proposedReplicas, "metricName", metricName, "reference", reference, "metric timestamp", metricTimestamp.Format(time.RFC1123))
 
 	rescaleMetric := ""
@@ -660,6 +661,9 @@ func (r *WatermarkPodAutoscalerReconciler) computeReplicas(ctx context.Context, 
 		replicas, metric, statuses, timestamp, readyReplicas, isAbove, isBelow, reason, err = r.computeReplicasWithRecommender(ctx, logger, wpa, scale)
 	} else {
 		replicas, metric, statuses, timestamp, readyReplicas, isAbove, isBelow, err = r.computeReplicasForMetrics(ctx, logger, wpa, scale)
+	}
+	if err != nil {
+		return 0, "", nil, time.Time{}, 0, false, err
 	}
 
 	condAbove := corev1.ConditionFalse
